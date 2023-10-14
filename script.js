afe099dc3ab0fe68b146e3d8d95a2ffbd6f91c5c
Amarjit Sahu <142235237+AmarjitSahu@users.noreply.github.com>
2023-10-14 03:39:43 +0000

    Some changes were made.

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ const view = () => {
     alert(`The bookmarks are ` + arr + ` . `);
 }
 const remove = () => {
-    r = prompt("Which bookmark do you want to remove? Type a number. ");
-    r = Number.parseInt(r);
+    do {
+        r = prompt("Which bookmark do you want to remove? Type a number. ");
+        r = Number.parseInt(r);
+        if (arr[r - 1] == undefined) {
+            alert("The bookmark does not exist. Try again");
+        }
+    } while (arr[r - 1] == undefined);
     arr.splice(r - 1, 1);
-    alert("The bookmark is deleted successfully. ");
+    if (arr[r - 1] == undefined) {
+        alert("The bookmark is deleted successfully. ");
+    }
+    else {
+        alert("The bookmark could not be removed. Please try again. ")
+    }
 }
 document.getElementById("view").addEventListener('click', view);
 document.getElementById("add").addEventListener('click', add);
